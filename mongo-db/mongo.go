e2ba36bfd81c0448cfae29b15469732e76c01ce2
Boburmirzo <symonbobur@gmail.com>
2024-07-05 12:09:21 +0500

    "Added mongoDB"

---
+++ mongo-db/mongo.go
@@ -0,0 +1,23 @@
+package mongodb
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+    
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+
+    err = client.Ping(context.TODO(), nil)
+    if err != nil {
+        panic(err)
+    }
+
+    return client 
+}
